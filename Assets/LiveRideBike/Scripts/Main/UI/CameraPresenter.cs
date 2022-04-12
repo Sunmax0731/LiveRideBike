29f434740b47061b2b3237977d0c7d322dd4edc7
Sunmax0731 <gkk.jhon@gmail.com>
2022-04-12 23:59:20 +0900

    カメラクラスを編集

--- Assets/LiveRideBike/Scripts/Main/UI/CameraPresenter.cs
+++ Assets/LiveRideBike/Scripts/Main/UI/CameraPresenter.cs
@@ -4,11 +4,16 @@ using Cysharp.Threading.Tasks;
 using Cysharp.Threading.Tasks.Triggers;
 using UniRx;
 using UnityEngine;
+using UnityEngine.UI;
 namespace Sunmax
 {
     public class CameraPresenter : MonoBehaviour
     {
         [SerializeField] private CameraSettings _CameraSettings;
+
+        [SerializeField] private Slider DistanceSlider;
+        [SerializeField] private Slider HeightSlider;
+        [SerializeField] private Slider AngleSlider;
 
         void Start()
         {
