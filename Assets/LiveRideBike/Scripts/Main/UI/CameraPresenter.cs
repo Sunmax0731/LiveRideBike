b6c9f1c8e5e4e6524fe3c0f34b845e74debc985a
Sunmax0731 <gkk.jhon@gmail.com>
2022-04-13 23:17:17 +0900

    Presenterクラスを修正

--- Assets/LiveRideBike/Scripts/Main/UI/CameraPresenter.cs
+++ Assets/LiveRideBike/Scripts/Main/UI/CameraPresenter.cs
@@ -5,6 +5,7 @@ using Cysharp.Threading.Tasks.Triggers;
 using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
+
 namespace Sunmax
 {
     public class CameraPresenter : MonoBehaviour
@@ -13,13 +14,33 @@ namespace Sunmax
 
         [SerializeField] private Slider DistanceSlider;
         [SerializeField] private Slider HeightSlider;
-        [SerializeField] private Slider AngleSlider;
+        [SerializeField] private Slider DepthSlider;
+        [SerializeField] private Button RegisterButton;
+        [SerializeField] private Button LoadButton;
+        [SerializeField] private Button InitButton;
+        private float DefaultDistance = 1.6f;
+        private Vector3 DefaultTargetPosition = new Vector3(0f, 0.75f, 0.2f);
 
         void Start()
         {
-            Observable.EveryUpdate()
-                .Where(_ => Input.GetKeyDown(KeyCode.C))
-                .Subscribe(_ => _CameraSettings.IncrementalCameraIndex());
+            DistanceSlider.OnValueChangedAsObservable().Subscribe(x =>
+            {
+                _CameraSettings.CameraList.ForEach(camera => camera.orthographicSize = DefaultDistance - x);
+            });
+
+            HeightSlider.OnValueChangedAsObservable().Subscribe(x =>
+            {
+                var pos = _CameraSettings.TargetPosition.Value;
+                pos.y = DefaultTargetPosition.y - x;
+                _CameraSettings.TargetPosition.Value = pos;
+            });
+
+            DepthSlider.OnValueChangedAsObservable().Subscribe(x =>
+            {
+                var pos = _CameraSettings.TargetPosition.Value;
+                pos.z = x + DefaultTargetPosition.z;
+                _CameraSettings.TargetPosition.Value = pos;
+            });
         }
     }
 }
--- Assets/LiveRideBike/Scripts/PlayerPrefs/PlayerPrefKey.cs
+++ Assets/LiveRideBike/Scripts/PlayerPrefs/PlayerPrefKey.cs
@@ -6,6 +6,7 @@ namespace Sunmax
 {
     static public class PlayerPref
     {
+        static public string UseCameraName = "UseCameraName";
         public enum BackgroundColorKey
         {
             CameraBackground_Red,
